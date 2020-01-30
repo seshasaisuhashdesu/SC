ed635ffa7d090c01b1a57bbe5700a9753e41b56d
16PW05@cs2k16.amcs.ws2k12.psgcs.edu <16PW05@cs2k16.amcs.ws2k12.psgcs.edu>
2020-01-30 15:19:20 -0600

    Added crossover

--- ga.r
+++ ga.r
@@ -1,5 +1,5 @@
 
-co_sites = c()
+co_sites = c(3,3,3,3)
 mut_sites = c()
 co_prob = 0.6
 mut_prob = 0.1
@@ -35,7 +35,8 @@ selected_chromosomes <- function(cum_Pi, r){
       }
       i = i + 1
     }
-    chrom_no = append(chrom_no, chrom)
+    if(is.null(chrom)) { chrom_no = append(chrom_no, 1) }
+    else{ chrom_no = append(chrom_no, chrom) }
   }
   
   return(chrom_no)
@@ -49,6 +50,28 @@ get_binary <- function(x){
   return(result)
 }
 
+
+do_crossover <- function(binary_pop){
+  selected_chrom_iter = 1
+  intermediate_population = c()
+  for(each in r_co){
+    if(each  <= co_prob){
+      slice1 = paste(substring(binary_pop[selected_chrom_iter],1,co_sites[(selected_chrom_iter+1)/2]), substring(binary_pop[selected_chrom_iter+1],co_sites[(selected_chrom_iter+1)/2]+1), sep="" )
+      slice2 = paste(substring(binary_pop[selected_chrom_iter+1],0,co_sites[(selected_chrom_iter+1)/2]), substring(binary_pop[selected_chrom_iter],co_sites[(selected_chrom_iter+1)/2]+1), sep="" )
+      
+      intermediate_population = append(intermediate_population, slice1)
+      intermediate_population = append(intermediate_population, slice2)
+    }
+    else{
+      intermediate_population = append(intermediate_population,binary_pop[selected_chrom_iter])
+      intermediate_population = append(intermediate_population,binary_pop[selected_chrom_iter+1])
+    }
+    selected_chrom_iter = selected_chrom_iter + 2
+  }
+  return(intermediate_population)
+}
+
+
 #Selection
 x_value = get_x_value(population)
 fx = obj_function(x_value)
@@ -61,4 +84,5 @@ selected = selected_chromosomes(cum_Pi,r_selection)
 #CrossOver
 r_co = runif(length(population)/2)
 binary_pop = get_binary(population[selected])
+intermediate_population = do_crossover(binary_pop)
 #Mutation
